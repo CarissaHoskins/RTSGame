a0dd53271c006a89b2328b68480a37514fbcdaa3
Carissa Hoskins <CJHoskins@student.fullsail.edu>
2018-10-07 16:48:16 -0400

    First Commit

---
+++ Assets/Scripts/BreakableBlockBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableBlockBehavior : MonoBehaviour
+{
+    private Color initial;
+    private Color highlighted;
+    private BoxCollider[] colliders;
+
+    public bool isSelceted = false;
+
+    [SerializeField] int health = 3;
+
+
+	void Start ()
+    {
+        initial = GetComponent<MeshRenderer>().material.color;
+        colliders = GetComponents<BoxCollider>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if (!isSelceted)
+        {
+            highlighted = GetComponent<MeshRenderer>().material.color;
+            highlighted.g = 255;
+            GetComponent<MeshRenderer>().material.color = highlighted;
+            isSelceted = true;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.color = initial;
+            isSelceted = false;
+        }
+    }
+
+    void Hit()
+    {
+        health -= 1;
+        if (health <= 0)
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                colliders[i].enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RoverBehavior.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverBehavior : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+    public float rotatingSpeed = 100f;
+
+    private bool isWandering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isWandering)
+        {
+            StartCoroutine("Wander");
+        }
+        if (isRotatingRight)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotatingSpeed); //transform.right
+        }
+        if (isRotatingLeft)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotatingSpeed); //transform.right
+        }
+        if (isWalking)
+        {
+            transform.position += (transform.forward * moveSpeed * Time.deltaTime);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Block")
+        {
+            isWalking = false;
+        }
+    }
+
+    IEnumerator Wander()
+    {
+        int RotateTime = Random.Range(1, 3);
+        int RotateWait = Random.Range(1, 3);
+        int RotateLorR = Random.Range(1, 3);
+        int WalkWait = Random.Range(1, 3);
+        int WalkTime = Random.Range(1, 3);
+
+        isWandering = true;
+
+        yield return new WaitForSecondsRealtime(WalkWait);
+        isWalking = true;
+        yield return new WaitForSecondsRealtime(WalkTime);
+        isWalking = false;
+        yield return new WaitForSecondsRealtime(RotateWait);
+        if (RotateLorR == 1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSecondsRealtime(RotateTime);
+            isRotatingRight = false;
+        }
+        if (RotateLorR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSecondsRealtime(RotateTime);
+            isRotatingLeft = false;
+        }
+        isWandering = false;
+    }
+}
